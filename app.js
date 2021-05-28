225d701809a4cba612145ddde1a054764d972b7f
Dasha <thisisdasha@gmail.com>
2021-05-28 09:54:51 +0300

    update readme

--- app.js
+++ app.js
@@ -1,132 +1,113 @@
-const http = require('http');
-const url = require('url');
-const fs = require('fs');
-const path = require('path');
+const http = require('http')
+const url = require('url')
+const fs = require('fs')
+const path = require('path')
 
-
-const port = process.env.PORT || 80;
-const data = fs.readFileSync(`${(__dirname)}/dev_data/data.json`);
-const dataArr = JSON.parse(data).services;
-let cardsUrlArr = [];
+const port = process.env.PORT || 80
+const data = fs.readFileSync(`${__dirname}/dev_data/data.json`)
+const dataArr = JSON.parse(data).services
+let cardsUrlArr = []
 
 dataArr.forEach(element => {
-    cardsUrlArr.push(element.alias)
-});
-
-
+  cardsUrlArr.push(element.alias)
+})
 
 /////TEMPLATES
 
-const mainTemp = fs.readFileSync(`${(__dirname)}/static/index.html`, 'utf-8');
-const cardTemp = fs.readFileSync(`${(__dirname)}/templates/cardTemp.html`, 'utf-8');
-const productTemp = fs.readFileSync(`${(__dirname)}/templates/productTemp.html`, 'utf-8');
-const errorPage = fs.readFileSync(`${(__dirname)}/templates/error.html`, 'utf-8');
-
+const mainTemp = fs.readFileSync(`${__dirname}/static/index.html`, 'utf-8')
+const cardTemp = fs.readFileSync(`${__dirname}/templates/cardTemp.html`, 'utf-8')
+const productTemp = fs.readFileSync(`${__dirname}/templates/productTemp.html`, 'utf-8')
+const errorPage = fs.readFileSync(`${__dirname}/templates/error.html`, 'utf-8')
 
 const sendRes = function (url, contentType, res) {
-    let file = path.join(__dirname + '/static/', url);
-    fs.readFile(file, (err, content) => {
-        if (err) {
-            res.writeHead(400);
-            console.log(`file ${file} not found 404`);
-            res.end(errorPage);
-        } else {
-            res.writeHead(200, {
-                'Content-Type': contentType
-            });
-            console.log(`file ${file} 200 OK`);
-            res.end(content);
-        }
-    })
-};
-
-
-
+  let file = path.join(__dirname + '/static/', url)
+  fs.readFile(file, (err, content) => {
+    if (err) {
+      res.writeHead(400)
+      console.log(`file ${file} not found 404`)
+      res.end(errorPage)
+    } else {
+      res.writeHead(200, {
+        'Content-Type': contentType,
+      })
+      console.log(`file ${file} 200 OK`)
+      res.end(content)
+    }
+  })
+}
 
 const getContentType = function (url) {
-    switch (path.extname(url)) {
-        case '.css':
-            return contentType = 'text/css';
-        case '.html':
-            return contentType = 'text/html';
-        case '.js':
-            return contentType = 'text/javascript';
-        case '.json':
-            return contentType = 'application/json';
-        case '.png':
-            return contentType = 'image/png';
-        default:
-            return contentType = 'application/octet-stream';
-    }
-};
+  switch (path.extname(url)) {
+    case '.css':
+      return (contentType = 'text/css')
+    case '.html':
+      return (contentType = 'text/html')
+    case '.js':
+      return (contentType = 'text/javascript')
+    case '.json':
+      return (contentType = 'application/json')
+    case '.png':
+      return (contentType = 'image/png')
+    default:
+      return (contentType = 'application/octet-stream')
+  }
+}
 
 const writeTemplate = function (template, product) {
-    let output = template.replace(/{%ALIAS%}/g, product.alias);
-    output = output.replace(/{%IMAGE%}/g, product.image);
-    output = output.replace(/{%TITLE%}/g, product.title);
-    output = output.replace(/{%DESCRIPTION%}/g, product.description);
-    output = output.replace(/{%PRICE%}/g, product.price);
-    output = output.replace(/{%TAGS%}/g, writeTags(product.properties));
-    return output;
-};
+  let output = template.replace(/{%ALIAS%}/g, product.alias)
+  output = output.replace(/{%IMAGE%}/g, product.image)
+  output = output.replace(/{%TITLE%}/g, product.title)
+  output = output.replace(/{%DESCRIPTION%}/g, product.description)
+  output = output.replace(/{%PRICE%}/g, product.price)
+  output = output.replace(/{%TAGS%}/g, writeTags(product.properties))
+  return output
+}
 
-const writeTags = function (properties) { //properties is array of obj
-    return properties.map(({
-        title,
-        value
-    }) => {
-        return `<a href="#" title=${title} class="btn btn-outline-primary btn-sm m-1" role="button">${value}</a>`;
-    }).join('');
-
-
-};
+const writeTags = function (properties) {
+  //properties is array of obj
+  return properties
+    .map(({ title, value }) => {
+      return `<a href="#" title=${title} class="btn btn-outline-primary btn-sm m-1" role="button">${value}</a>`
+    })
+    .join('')
+}
 
 const detectCard = function (productAlias) {
-    let product = dataArr.filter(function (el) {
-        if (el.alias === productAlias) {
-            return el;
-        }
-    })
-    return product[0];
-
-};
-
-
+  let product = dataArr.filter(function (el) {
+    if (el.alias === productAlias) {
+      return el
+    }
+  })
+  return product[0]
+}
 
 ///////SERVER
 
 const server = http.createServer((req, res) => {
-    let reqParams = url.parse(req.url, true);
-    const {
-        pathname
-    } = reqParams;
+  let reqParams = url.parse(req.url, true)
+  const { pathname } = reqParams
 
-    if (pathname === '/' || pathname === '/main') {
-        res.writeHead(200, {
-            'Content-Type': 'text/html'
-        });
-        const cardsHtml = dataArr.map(el => writeTemplate(cardTemp, el)).join('');
-        const output = mainTemp.replace('{%PRODUCT_CARD%}', cardsHtml);
-        res.end(output);
-        // sendRes('./index.html', {
-        //     'Content-Type': 'text/html'
-        // }, res);
-    } else if (cardsUrlArr.includes(`${pathname.slice(1)}`)) {
-        res.writeHead(200, {
-            'Content-Type': 'text/html'
-        });
-        const productAlias = pathname.slice(1);
-        const product = detectCard(productAlias);
-        const productHtml = writeTemplate(productTemp, product);
-        const output = mainTemp.replace('{%PRODUCT_CARD%}', productHtml);
-        //path = dataArr[query.alias];
-        res.end(output);
-    } else {
-        sendRes(req.url, getContentType(req.url), res);
-    }
-
-});
+  if (pathname === '/' || pathname === '/main') {
+    res.writeHead(200, {
+      'Content-Type': 'text/html',
+    })
+    const cardsHtml = dataArr.map(el => writeTemplate(cardTemp, el)).join('')
+    const output = mainTemp.replace('{%PRODUCT_CARD%}', cardsHtml)
+    res.end(output)
+  } else if (cardsUrlArr.includes(`${pathname.slice(1)}`)) {
+    res.writeHead(200, {
+      'Content-Type': 'text/html',
+    })
+    const productAlias = pathname.slice(1)
+    const product = detectCard(productAlias)
+    const productHtml = writeTemplate(productTemp, product)
+    const output = mainTemp.replace('{%PRODUCT_CARD%}', productHtml)
+    res.end(output)
+  } else {
+    sendRes(req.url, getContentType(req.url), res)
+  }
+})
 
 server.listen(port, () => {
-    console.log(`Server working on port ${port}!`);
+  console.log(`Server working on port ${port}!`)
 })
---
+++ readme.md
@@ -0,0 +1,15 @@
+##Сайт на NodeJs
+
+можно увидеть [здесь](https://fitness-market.herokuapp.com/)
+
+Данные берутся из `data.json`, вёрстка на `Bootstrap`
+
+## Установка
+
+    $ git clone https://github.com/Mango-Tango19/NodeJs_SPA
+    $ cd NodeJs_SPA
+    $ npm install
+
+## Запустить проект
+
+    $ npm start
